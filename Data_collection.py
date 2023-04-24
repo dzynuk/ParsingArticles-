89e57745fd8f3ecd2a108da35902d2c79d4b5c3a
Mykola <118020035+dzynuk@users.noreply.github.com>
2023-04-24 14:59:52 +0300

    Add files via upload

---
+++ Data_collection.py
@@ -0,0 +1,39 @@
+# This code downloads a large number of XML files from a specific URL using the requests library in Python.
+# The code creates a list of file URLs to download, and then iterates through the list, downloading each file using urllib.request.urlretrieve() function.
+# The downloaded files are saved in a folder called 'folderofarticles'. If the folder does not exist, it is created using os.makedirs() function.
+
+import requests
+import os
+import urllib.request
+
+url = "https://ftp.ncbi.nlm.nih.gov/pubmed/baseline/"
+headers = dict(Accept="*/*")
+req = requests.get(url, headers=headers)
+src = req.text
+
+# generator url_list
+url_list = []
+for i in range(1, 1167):
+    count = str(i).zfill(4)
+    url = f"https://ftp.ncbi.nlm.nih.gov/pubmed/baseline/pubmed23n{count}.xml.gz"
+    url_list.append(url)
+
+if not os.path.exists('folderofarticles'):
+    os.makedirs('folderofarticles')
+
+    # Iterate through the list of file URLs and download each file
+for url in url_list:
+    file_name = os.path.basename(url)
+    file_path = os.path.join('folderofarticles', file_name)
+    urllib.request.urlretrieve(url, file_path)
+
+
+
+
+
+
+
+
+
+
+
